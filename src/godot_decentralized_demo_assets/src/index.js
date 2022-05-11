0feb390e7b24a0047e25d2f4c3340c9b53ccdc1d
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-05-11 03:21:01 +0000

    Initial commit.

---
+++ src/godot_decentralized_demo_assets/src/index.js
@@ -0,0 +1,19 @@
+import { godot_decentralized_demo } from "../../declarations/godot_decentralized_demo";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await godot_decentralized_demo.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
